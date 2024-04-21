1e1019ddd384514ef536c31b1c30bc51c2c5a236
Steffen Körner <steffen.koerner@mytum.de>
2024-04-21 15:44:35 +0200

    Update packages.sh

--- shell/packages.sh
+++ shell/packages.sh
@@ -6,3 +6,11 @@ sudo apt install htop
 sudo apt install neovim
 sudo apt install git-gui
 sudo apt install bat # Improved version of cat that has syntax highlighting.
+sudo apt install pip
+
+
+
+pip install tldr # an alternative to man command that tries to be more useful
+
+
+
